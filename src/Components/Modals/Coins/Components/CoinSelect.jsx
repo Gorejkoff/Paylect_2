92d244fc2d09caa9a14f9224bf4ad49eca4b6514
gorejkoff <neskazua63@gmail.com>
2025-10-14 13:15:02 +0300

    change path inage

--- src/Components/Modals/Coins/Components/CoinSelect.jsx
+++ src/Components/Modals/Coins/Components/CoinSelect.jsx
@@ -7,7 +7,7 @@ export default function CoinSelect({ text, img, alt, active }) {
    return (
       <S.SelectCoin>
          <input type="radio" name="coin_select" />
-         <S.SelectImg src="/svg/arrow-color.svg" alt="selected" />
+         <S.SelectImg src="./svg/arrow-color.svg" alt="selected" />
          {text}
          <S.CoinIcon src={img} alt={alt} />
       </S.SelectCoin>
